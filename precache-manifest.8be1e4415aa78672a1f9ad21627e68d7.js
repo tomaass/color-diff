e3fd5fadef385a54c98f3d1186e7d69d09ad9f23
tomaass <tomas@sorejs.cz>
2020-03-09 08:20:10 +0100

    Updates

---
+++ precache-manifest.8be1e4415aa78672a1f9ad21627e68d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a439ad17d7f3c07d2b10bc94c9f9b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58df86460c00d97cbead",
+    "url": "/static/css/main.5c89ad73.chunk.css"
+  },
+  {
+    "revision": "bda058c9dceb46d34a70",
+    "url": "/static/js/2.78dc52f9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.78dc52f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58df86460c00d97cbead",
+    "url": "/static/js/main.56e44cd8.chunk.js"
+  },
+  {
+    "revision": "a387b455e6b94a7f027d",
+    "url": "/static/js/runtime-main.b3d82c4a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
