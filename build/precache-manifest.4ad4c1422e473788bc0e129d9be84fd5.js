0c72b6ea96b83ca94c3fd29f86a1a42e9ef7bf6e
tomaass <tomas@sorejs.cz>
2020-03-09 08:22:57 +0100

    Add gh build

---
+++ build/precache-manifest.4ad4c1422e473788bc0e129d9be84fd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc03fa2c53eb2a30f36018cb2c2423e",
+    "url": "/color-diff/index.html"
+  },
+  {
+    "revision": "69e8ee2907e862eb87f4",
+    "url": "/color-diff/static/css/main.5c89ad73.chunk.css"
+  },
+  {
+    "revision": "bda058c9dceb46d34a70",
+    "url": "/color-diff/static/js/2.78dc52f9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/color-diff/static/js/2.78dc52f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69e8ee2907e862eb87f4",
+    "url": "/color-diff/static/js/main.91b2aa43.chunk.js"
+  },
+  {
+    "revision": "0cd6b9e3d60eefc07c71",
+    "url": "/color-diff/static/js/runtime-main.4d39f45a.js"
+  }
+]);
